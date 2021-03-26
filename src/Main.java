7073314ab3a9314cca66fc4658d34a80ab6f06c2
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-03-26 04:46:06 -0400

    Added sound when launching application

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,25 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.MediaPlayer;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.util.Duration;
+
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Main extends Application {
+
+
     public static void main(String[] args) {
         launch(args);
     }
 
-    public void start(Stage primaryStage) throws IOException, InterruptedException {
+    public void start(Stage primaryStage) throws IOException, InterruptedException, URISyntaxException {
+        AudioClip startSound = new AudioClip("file:src/assets/audio/brain_teaser_start.wav");
         Parent root = FXMLLoader.load(getClass().getResource("screens/FXMLMainscreen.fxml"));
         Image logo = new Image("/assets/images/icons/loadingscreen_logo.png");
         ImageView logoView = new ImageView();
@@ -32,6 +40,7 @@ public class Main extends Application {
         primaryStage.setTitle("Brain Teaser");
         primaryStage.setResizable(false);
         primaryStage.show();
+        startSound.play();
         PauseTransition loadingDelay = new PauseTransition(Duration.seconds(2));
         loadingDelay.setOnFinished(event -> primaryStage.setScene(scene));
         loadingDelay.play();
