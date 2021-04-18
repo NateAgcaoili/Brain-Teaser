a763fe3dc3b59a458e66d8fbda73880892b980dd
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-04-18 19:50:48 -0400

    Added options button and play again/main menu buttons when game ends

--- src/games/simonsays/SimonSaysMain.java
+++ src/games/simonsays/SimonSaysMain.java
@@ -1,8 +1,14 @@
 package games.simonsays;
 
+import games.GameOptions;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -11,6 +17,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -27,6 +34,9 @@ public class SimonSaysMain extends Application {
     boolean playerTurn;
     Text roundDisplay;
     Text message;
+    Button playAgainButton;
+    Button mainMenuButton;
+    Button optionsButton;
     GameButton[] gameButtons;
     Color[] colors;
     ArrayList<Integer> simonSequence = new ArrayList<>();
@@ -54,6 +64,34 @@ public class SimonSaysMain extends Application {
         message.setText("Simon's Turn");
         message.setX(480);
         message.setY(620);
+        playAgainButton = new Button("PLAY AGAIN");
+        playAgainButton.setOnAction(e -> onClickPlayAgain(e));
+        playAgainButton.setPrefSize(200, 80);
+        playAgainButton.setLayoutX(480);
+        playAgainButton.setLayoutY(630);
+        playAgainButton.setVisible(false);
+        mainMenuButton = new Button("MAIN MENU");
+        mainMenuButton.setOnAction(e -> {
+            try {
+                onClickMainMenu(e);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+        mainMenuButton.setPrefSize(200, 80);
+        mainMenuButton.setLayoutX(690);
+        mainMenuButton.setLayoutY(630);
+        mainMenuButton.setVisible(false);
+        optionsButton = new Button("OPTIONS");
+        optionsButton.setOnAction(e -> {
+            try {
+                openOptions(e);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+        optionsButton.setLayoutX(10);
+        optionsButton.setLayoutY(10);
         GameButton butOne = new GameButton(0,480, 150, Color.BLUE);
         GameButton butTwo = new GameButton(1,615, 150, Color.BLUE);
         GameButton butThree = new GameButton(2,750, 150, Color.BLUE);
@@ -68,7 +106,7 @@ public class SimonSaysMain extends Application {
         for(int i = 0; i < 9; i++) {
             root.getChildren().add(gameButtons[i]);
         }
-        root.getChildren().addAll(roundDisplay, message);
+        root.getChildren().addAll(roundDisplay, message, playAgainButton, mainMenuButton, optionsButton);
         playGame();
         return root;
     }
@@ -105,8 +143,57 @@ public class SimonSaysMain extends Application {
     public void gameOver() {
         playerTurn = false;
         setButtonColors(colors[3]);
+        playAgainButton.setVisible(true);
+        mainMenuButton.setVisible(true);
         message.setText("Game Over");
         roundDisplay.setText("Score: " + currentRound);
+    }
+
+    private void openOptions(ActionEvent event) throws IOException {
+        int result = GameOptions.display();
+        switch (result) {
+            case 0:
+                System.out.println("Returned");
+                break;
+            case 1:
+                Parent gameParent = FXMLLoader.load(getClass().getResource("/screens/GameScreen.fxml"));
+                Scene gameScene = new Scene(gameParent);
+
+                // getting stage information
+                Stage gameWindow = (Stage)((Node)event.getSource()).getScene().getWindow();
+                gameWindow.setScene(gameScene);
+                gameWindow.show();
+                break;
+            case 2:
+                Parent root = FXMLLoader.load(getClass().getResource("/screens/FXMLMainscreen.fxml"));
+                Stage homeWindow = (Stage)((Node)event.getSource()).getScene().getWindow();
+                Scene home = new Scene(root);
+                homeWindow.setScene(home);
+                homeWindow.show();
+                break;
+            default:
+                System.out.println("Unknown");
+        }
+    }
+
+    public void onClickPlayAgain(ActionEvent event) {
+        SimonSaysMain simonSays = new SimonSaysMain();
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        try {
+            simonSays.start(window);
+        } catch (Exception e) {
+            e.printStackTrace();
+        };
+        window.show();
+    }
+
+    public void onClickMainMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/screens/FXMLMainscreen.fxml"));
+        Stage homeWindow = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene home = new Scene(root);
+        homeWindow.setScene(home);
+        homeWindow.show();
     }
 
     public class GameButton extends StackPane {
