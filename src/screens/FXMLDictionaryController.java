027a17a6cccaca97a39c758409763c5dc4c1f85e
pc02130 <campbell.perry33@gmail.com>
2021-04-20 14:36:25 -0400

    Changed some small UI elements in settings, dictionary and dict controller

--- src/screens/FXMLDictionaryController.java
+++ src/screens/FXMLDictionaryController.java
@@ -10,6 +10,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -25,7 +27,9 @@ public class FXMLDictionaryController {
         WordReader reader = new WordReader("/dictionary/dictionary.txt");
 
         for (String word : reader.getWordsWithDefinition()) {
-            Label label = new Label(word);
+            String capitalize = word.substring(0,1).toUpperCase();
+            Label label = new Label(capitalize + word.substring(1));
+            label.setFont(Font.font("System", FontWeight.BOLD, 40));
             vbox.getChildren().add(label);
             vbox.getChildren().add(new Separator());
         }
